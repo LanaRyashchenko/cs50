97a4fc63b1d3a14f17ff7a021571a83a4d965b57
LanaRyashchenko <siliel.ua@gmail.com>
2017-02-02 23:00:07 +0200

    using functions
    added functions main() and check()

--- mario.py
+++ mario.py
@@ -1,12 +1,18 @@
 import cs50
 import sys
 
-height = int(input("Height: "))
-while height < 0 or height > 23:
-    height = int(input("Retry "))
+def check():
+    height = int(input("Height: "))
+    while height < 0 or height > 23:
+        height = int(input("Retry "))
     if height == 0:
         print("0")
-else:
-    for n in range(1,height+1):
-        print(' ' * (height - n), end = '')
-        print('#' * (n+1))
+    else: 
+        for n in range(1,height+1):
+            print(' ' * (height - n), end = '')
+            print('#' * (n+1))
+def main():
+    check()
+        
+if __name__ == "__main__":
+    main()
