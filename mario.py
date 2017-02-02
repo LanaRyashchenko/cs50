2d60f3f1aaa82b5bc4c2c864194d5d77808f8591
LanaRyashchenko <siliel.ua@gmail.com>
2017-02-02 22:35:45 +0200

    Delete empty line

--- mario.py
+++ mario.py
@@ -1,7 +1,6 @@
 import cs50
 import sys
 
-
 height = int(input("Height: "))
 while height < 0 or height > 23:
     height = int(input("Retry "))
@@ -11,9 +10,3 @@ else:
     for n in range(1,height+1):
         print(' ' * (height - n), end = '')
         print('#' * (n+1))
-       
-
-
-        
-    
-
