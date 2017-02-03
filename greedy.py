0c05873f0d124f7e67a771cf66a070e4a261e440
Lana <siliel.ua@gmail.com>
2017-02-03 12:56:44 +0200

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -1,36 +1,30 @@
 import cs50
 import sys
 
-coins = 0
-
 def check(money):
     while money == 0 and money < 0:
         print("Please put amount in $")
         money = cs50.get_float()
-       
-def calculation(cents):
-    global coins
-    while coins >= 25:
-        +coins
+    cents = round(money*100)
+    coins = 0
+    while cents >= 25:
+        coins = coins + 1
         cents = cents - 25
-    while coins >= 10:
-        +coins
+    while cents >= 10:
+        coins = coins + 1
         cents = cents - 10
-    while coins >= 5:
-        +coins
+    while cents >= 5:
+        coins = coins + 1
         cents = cents - 5
-    while coins >= 1:
-        +coins
+    while cents >= 1:
+        coins = coins + 1
         cents = cents - 1
-    return coins
     print(coins)
         
 def main():
     print("O hai! How much change is owed?")
     money = cs50.get_float()
     check(money)
-    cents = round(money*100)
-    calculation(cents)
-          
+
 if __name__ == "__main__":
     main()
