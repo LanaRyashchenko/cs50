102f50ccb4177e47e072fe7c358335a890fd1e35
Lana <siliel.ua@gmail.com>
2017-02-08 21:14:33 +0200

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -10,11 +10,15 @@ key = sys.argv[1]
 print("please, enter a text:")     
 plaintext = cs50.get_string()
 
-for i in filter(str.isalpha, plaintext):
-    if(i.islower()):
-        ciphertext = ((ord(i) - 97) + int(key)) %26 + 97
-        print(chr(ciphertext), end="")
-    if(i.isupper()):
-        ciphertext = ((ord(i) - 65) + int(key)) %26 + 65
-        print(chr(ciphertext), end="")
+for i in plaintext:
+    if(i.isalpha()):
+        if(i.islower()):
+            ciphertext = ((ord(i) - 97) + int(key)) %26 + 97
+            print(chr(ciphertext), end="")
+        else: #if(i.isupper()):
+            ciphertext = ((ord(i) - 65) + int(key)) %26 + 65
+            print(chr(ciphertext), end="")
+    else:
+        print(i, end="")
+print()
  
